98a6e84aa80cd85cc87a291bff123b5ebea25216
Mai Houa Vue <mhvue4@gmail.com>
2020-10-04 21:48:33 -0500

    added form validation to not allow blank entries. attempted to fix size of container so scrollbar isn't out too far

--- public/app.js
+++ public/app.js
@@ -3,8 +3,6 @@ let numberTask=0;
 $("#btn").on("click", function(event){
     //prevent the default action of the button on this form
     event.preventDefault();
-    //numberTask will increase to allow numbered list in tasks entered
-    numberTask++; 
 
     //grabs the text inputted
     const text = $("#taskName").val().trim()
@@ -12,22 +10,30 @@ $("#btn").on("click", function(event){
     //creating a delete button
     const doneBtn= $("<button>").text("Done").addClass("deleteTask")
 
+    if(text) {
+        $("#cannotBlank").remove();
+        //numberTask will increase to allow numbered list in tasks entered
+        numberTask++; 
 
-    //clearing our input box 
-    $("#taskName").val(" ");
+        //variable to hold the text entered by user, with a class and id 
+        const taskInfo = $("<p>").text(numberTask + ")" + text).addClass("textList").attr("id","taskNo."+numberTask)
 
-    //variable to hold the text entered by user, with a class and id 
-    const taskInfo = $("<p>").text(numberTask + ")" + text).addClass("textList").attr("id","taskNo."+numberTask)
+        //append the input text to the div, allow it to show 
+        $(".taskContainer").append(taskInfo, doneBtn);
 
-    //append the input text to the div, allow it to show 
-    $(".taskContainer").append(taskInfo, doneBtn);
+        //option to strike out the tasks
+        $(".deleteTask").on("click",function(){
+            //need to grab that speific task then change class to striked and using css, have it strike out that tasks 
+            const taskNoInfo= $(this).prev().removeClass("textList").addClass("striked")
+                        
+        });
 
-    //option to strike out the tasks
-    $(".deleteTask").on("click",function(){
-        //need to grab that speific task then change class to striked and using css, have it strike out that tasks 
-        const taskNoInfo= $(this).prev().removeClass("textList").addClass("striked")
-                    
-    });
+        //clearing our input box 
+        $("#taskName").val(" ");
+    }
+    else{
+        $("#taskName").before("<span id='cannotBlank'>Cannot be blank</span>");
+    }
 
 });
 
@@ -37,13 +43,14 @@ $("#clearBtn", document.body).on("click", function(){
      //adding msg if there are empty div in the taskContainer but clear all btn was pressed
      if($(".taskContainer").contents().length == 0){
         console.log("empty");
+        // want to add modal to show msg that div is empty = nothing to clear
     }
     else{
-    //delete everything in the taskContainer
-    $(".textList, .striked,.deleteTask").remove();
+        //delete everything in the taskContainer
+        $(".textList, .striked,.deleteTask").remove();
 
-    //resting numbers back to 0 so a new list can start with correct numerical order again
-    numberTask=0;
+        //resting numbers back to 0 so a new list can start with correct numerical order again
+        numberTask=0;
     }
    
 })
@@ -52,3 +59,4 @@ $("#clearBtn", document.body).on("click", function(){
 //need to do:
     //incorpoate localStorage 
     //user msg if taskContainer is empty via modal
+    //user msg form validation
