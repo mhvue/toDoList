c3e12793fa6c40f21b974aef560aef872f953322
Mai Houa Vue <mhvue4@gmail.com>
2020-10-12 22:27:46 -0500

    added li to be removed when Clear all btn pressed. updated readMe with deployed link of app

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # toDoList
 
-Creating a to do list to help myself stay organized.
+Creating a to do list to help myself stay organized:  https://mhvue.github.io/toDoList/
+
+Using localStorage to persist data.
--- public/app.js
+++ public/app.js
@@ -1,14 +1,20 @@
 let taskArr=JSON.parse(localStorage.getItem("taskInfo")) || [];
 
-
+//adding a task 
 function addTasks(task){
+    //have a const to hold our listed items (which will be numbered)
     const info="<li><span>" + task + "</span><button class='deleteTask'>Done</button></li>";
+
+    //append those li to our taskList (which is ol on html side)
     $("#taskList").append(info); 
 }
 
+//saving task to local storage 
 function saveTaskData(){
     //console.log(this)
     //scan the list for objects that are not removed 
+
+    //either way can work below:
     // $("#taskList > li") //direct children
     // $("#taskList  li") //desecent children
     let arr=[];
@@ -19,16 +25,17 @@ function saveTaskData(){
         arr.push($(this).text());
     });
 
-    //set local storage
+    //set local storage with these items now 
     localStorage.setItem("taskInfo", JSON.stringify(arr))
 }
 
-
+//loop through task array 
 for(let i = 0; i < taskArr.length; i++){
+    //call addtask func with all the items from task array so the function can show user all the tasks 
     addTasks(taskArr[i]);
 }
 
- 
+ //when pressing submit btn when done inputing a task  
 $("#btn").on("click", function(event){
     //prevent the default action of the button on this form
     event.preventDefault();
@@ -37,12 +44,15 @@ $("#btn").on("click", function(event){
     const text = $("#taskName").val().trim();
 
     if(text) {
+        //removeing msg of cannot be blank if user enters text
         $("#cannotBlank").remove();
+        //remove the msg to user informing there's no task added so far 
         $("#nothingMsg").remove();
         
         addTasks(text);
 
         saveTaskData();
+
         //clearing our input box 
         $("#taskName").val(" ");
     }
@@ -55,9 +65,8 @@ $("#btn").on("click", function(event){
 
  // strike out the tasks
 $(".deleteTask").on("click",function(){
-    //need to grab that speific task then change class to striked and using css, have it strike out that tasks 
-    console.log("click")
-    // want to remove the item from array 
+    //console.log(this)
+    //finding the item to be stricked 
     $(this).parent().addClass("striked");
 
     saveTaskData();
@@ -65,33 +74,27 @@ $(".deleteTask").on("click",function(){
 
 
 
-//clear task list 
+//clear ALL the tasks in the list 
 $("#clearBtn", document.body).on("click", function(){
-     //adding msg if there are empty div in the taskContainer but clear all btn was pressed
+     //adding msg if there are no tasks listed, but clear all btn was pressed by user
     //credit If statement to https://stackoverflow.com/questions/6813227/how-do-i-check-if-an-html-element-is-empty-using-jquery/18488130
-    if(($.trim($(".taskContainer").html())=='')){
-        console.log("empty");
-        $(".taskContainer").html("<span id='nothingMsg'>Nothing to delete</span>")
+    if(($.trim($("#taskList").html())=='')){
+        $("#taskList").html("<span id='nothingMsg'>Nothing to delete</span>")
     }
     else{
         //show modal asking if user is sure to delete everything
         $(".deleteModal").modal();
-        //delete everything in the taskContainer
-        $(".yesBtn").on("click", function(){
-            localStorage.clear();
 
-            if(localStorage == null){
-               $(".taskContainer").text("")
-           }
+        //when click on Yes to agree on deleting everything, then delete everything tasks listed and clear local storage
+        $(".yesBtn").on("click", function(){
             //delete everything in the taskContainer
-             $(".striked,.deleteTask").remove();
-            
+             $(".striked,.deleteTask, li").remove();
+
+             //clear local storage 
+             localStorage.clear();
          });
        
     }
    
 })
 
-
-//need to do:
- //work on clear all due to added functions above on 10/11/20. Clear all now only deletes the Delete button
